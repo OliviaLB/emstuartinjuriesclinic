a374d1d3c190e3e4a4545a7947fa641a06c2e346
OliviaLB <olivia.l.bates@gmail.com>
2024-04-04 23:08:58 +0100

    Add build

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,12 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
+			<head>
+				<link
+					rel="icon"
+					href="/icon.png"
+				/>
+			</head>
 			<body>{children}</body>
 		</html>
 	);
