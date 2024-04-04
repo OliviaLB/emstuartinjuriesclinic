eca6a7d9f3d83dd16d8b8dfe2ed6c3cd87d9de5b
OliviaLB <olivia.l.bates@gmail.com>
2024-04-04 22:23:34 +0100

    Set up project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+export const metadata = {
+	title: 'App Router',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang="en">
+			<body>{children}</body>
+		</html>
+	);
 }
